313ed6d9ccd2eab3617fb9ccc52eca81547f96d3
Baljeet-68 <baljeet.saini@mmfinfotech.in>
2025-12-06 16:58:32 +0530

    Fix quoting in deploy script for improved reliability

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-REPO=/home/mmfilgqi/public_html/Project_Tracker_Tool
-TARGET=/home/mmfilgqi/public_html/Project_Tracker_Tool
+REPO="/home/mmfilgqi/public_html/Project_Tracker_Tool"
+TARGET="/home/mmfilgqi/public_html/Project_Tracker_Tool"
 
 echo "=== DEPLOY START ==="
 
 echo "Copying frontend build..."
-rm -f $TARGET/index.html
-rm -rf $TARGET/assets
+rm -f "$TARGET/index.html"
+rm -rf "$TARGET/assets"
 
-cp $REPO/client/dist/index.html $TARGET/index.html
-cp -r $REPO/client/dist/assets $TARGET/assets
+cp "$REPO/client/dist/index.html" "$TARGET/index.html"
+cp -r "$REPO/client/dist/assets" "$TARGET/assets"
 
 echo "Copying backend..."
-rm -rf $TARGET/server
-cp -r $REPO/server $TARGET/server
+rm -rf "$TARGET/server"
+cp -r "$REPO/server" "$TARGET/server"
 
 echo "=== DEPLOY COMPLETE ==="
