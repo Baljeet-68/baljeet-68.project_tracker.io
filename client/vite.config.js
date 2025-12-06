71bdc2edc96ed94a27172114e0a769de71293bba
Baljeet-68 <baljeet.saini@mmfinfotech.in>
2025-12-06 16:49:08 +0530

    Deploy routing fix + updated build

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Project_Tracker_Tool/',   // ✔ required for assets
-})
-
-
+  base: '/Project_Tracker_Tool/',
+});
