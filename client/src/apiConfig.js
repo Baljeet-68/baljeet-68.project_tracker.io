96a2934bd3224520a2efc2c785707c8817be8771
Baljeet-68 <baljeet.saini@mmfinfotech.in>
2025-12-09 15:27:26 +0530

    fixe the URL issue

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -7,7 +7,7 @@ const LOCAL_API_URL = "http://localhost:4000";
 // LIVE API BASE (must include /server/api)
 const LIVE_API_URL =
   import.meta.env.VITE_API_URL ||
-  "https://mmfinfotech.website/Project_Tracker_Tool/server/api";
+  "https://mmfinfotech.website/Project_Tracker_Tool/server";
 
 // FINAL BASE URL
 export const API_BASE_URL = USE_LIVE_API ? LIVE_API_URL : LOCAL_API_URL;
