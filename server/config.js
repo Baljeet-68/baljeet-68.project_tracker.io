6fa7f80ea18caaf85e73327f15e8582a4fd2c318
Baljeet-68 <baljeet.saini@mmfinfotech.in>
2025-12-19 12:59:56 +0530

    edit config

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 // Toggle this to switch between local and live DB
 const USE_LIVE_DB = true; // true = MariaDB, false = local
 
-const USE_ENCRYPTION = true; // true = encrypt sensitive data, false = no encryption
+const USE_ENCRYPTION = false; // true = encrypt sensitive data, false = no encryption
 
 module.exports = {
   USE_LIVE_DB,
