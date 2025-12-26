381382ea236c1f3ddcb246c849713de1ef801f05
Baljeet-68 <baljeet.saini@mmfinfotech.in>
2025-12-26 17:10:52 +0530

    feat: switch to live environment configuration
    Update API and database configurations to use live environment settings instead of local. This includes enabling live API endpoints, modifying database connection logic, and updating environment variables.

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 // Toggle this to switch between local and live DB
-const USE_LIVE_DB = process.env.MODE === 'false'; // Live = MariaDB, false = local #for live
+const USE_LIVE_DB = process.env.MODE === 'live'; // Live = MariaDB, false = local #for live
 
 const USE_ENCRYPTION = process.env.USE_ENCRYPTION === 'true'; // true = encrypt sensitive data, false = no encryption
 
