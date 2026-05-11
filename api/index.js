aad9007100d329ffb6cba44f12ae8d9ebc2d64a1
AbhiPatodi <https://github.com/AbhiPatodi/zumi-marketing-toolkit.git>
2026-05-11 19:02:42 +0530

    debug: surface exact boot error with env var presence check

--- api/index.js
+++ api/index.js
@@ -1,1 +1,14 @@
-module.exports = require('../server/app');
+let app;
+try {
+  app = require('../server/app');
+} catch (err) {
+  app = (req, res) => res.status(500).json({
+    boot_error: err.message,
+    type: err.name,
+    has_jwt: !!process.env.JWT_SECRET,
+    has_origin: !!process.env.PUBLIC_APP_ORIGIN,
+    mode: process.env.MODE || 'unset',
+    base_url: process.env.BASE_URL || 'unset',
+  });
+}
+module.exports = app;
