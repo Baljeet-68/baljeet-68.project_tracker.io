2c5a978d9a993614f164f9b7855c04dff42e6999
AbhiPatodi <https://github.com/AbhiPatodi/zumi-marketing-toolkit.git>
2026-05-11 19:27:45 +0530

    debug: re-add CORS error handler to surface crash message

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
-module.exports = require('../server/app');
+let app;
+try {
+  app = require('../server/app');
+} catch (err) {
+  app = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    if (req.method === 'OPTIONS') return res.status(200).end();
+    res.status(500).json({ boot_error: err.message, type: err.name });
+  };
+}
+module.exports = app;
