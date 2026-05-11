80eeb3dfffb19a5424b8efd812c9ee1a6366ae69
AbhiPatodi <https://github.com/AbhiPatodi/zumi-marketing-toolkit.git>
2026-05-11 19:28:59 +0530

    debug: lazy-load server app so CORS headers always send before crash details

--- api/index.js
+++ api/index.js
@@ -1,13 +1,31 @@
-let app;
-try {
-  app = require('../server/app');
-} catch (err) {
-  app = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    if (req.method === 'OPTIONS') return res.status(200).end();
-    res.status(500).json({ boot_error: err.message, type: err.name });
-  };
+let app = null;
+let bootError = null;
+
+function cors(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 }
-module.exports = app;
+
+module.exports = (req, res) => {
+  cors(res);
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  if (!app && !bootError) {
+    try {
+      app = require('../server/app');
+    } catch (err) {
+      bootError = err;
+    }
+  }
+
+  if (bootError) {
+    return res.status(500).json({
+      boot_error: bootError.message,
+      type: bootError.name,
+      stack: bootError.stack,
+    });
+  }
+
+  return app(req, res);
+};
