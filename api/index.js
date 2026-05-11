231f5c1e72e213925502421f3fd1050c1117eff6
AbhiPatodi <https://github.com/AbhiPatodi/zumi-marketing-toolkit.git>
2026-05-11 17:19:05 +0530

    debug: catch boot error in api/index.js and surface it as JSON

--- api/index.js
+++ api/index.js
@@ -1,1 +1,9 @@
-module.exports = require('../server/app');
+let app;
+try {
+  app = require('../server/app');
+} catch (err) {
+  app = (req, res) => {
+    res.status(500).json({ boot_error: err.message, stack: err.stack });
+  };
+}
+module.exports = app;
